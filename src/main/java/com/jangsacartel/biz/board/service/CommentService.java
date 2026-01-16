2de4f1d23188416d4b31276f208afcaae204ac9d
starwhale0w0 <starwhale@bu.ac.kr>
2026-01-16 20:24:45 +0900

    ♻️Refactor : 몇몇 알림창 모달창으로 리팩 및 좋아요 기능 리팩토링

--- src/main/java/com/jangsacartel/biz/board/service/CommentService.java
+++ src/main/java/com/jangsacartel/biz/board/service/CommentService.java
@@ -6,8 +6,7 @@ import com.jangsacartel.biz.board.dto.CommentDTO;
 
 public interface CommentService {
 
-    List<CommentDTO> getCommentsByPostId(int postId);
-    
+    	List<CommentDTO> getCommentsByPostId(int postId, Integer userId);    
     CommentDTO getCommentById(int commentId);
 
     void createComment(CommentDTO comment);
